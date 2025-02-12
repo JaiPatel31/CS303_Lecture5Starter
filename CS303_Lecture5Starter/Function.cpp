40f8e79c0b18589ba6aba97eacb0f25652d3982a
JaiPatel31 <149627486+JaiPatel31@users.noreply.github.com>
2025-02-12 11:52:10 -0600

    lecture 5 snow day

--- CS303_Lecture5Starter/Function.cpp
+++ CS303_Lecture5Starter/Function.cpp
@@ -5,8 +5,12 @@
 //POST: prints through the list
 void traversePrint(Node<string>* head) {
     //set temp ptr
-
+	Node<string>* currentNode = head;
     //loop through linked list & print data
+	while (currentNode != nullptr) {
+		cout << currentNode->data << endl;
+		currentNode = currentNode->next;
+	}
 }
 
 //TASK 2
@@ -22,17 +26,24 @@ void addBefore(Node<string>* head, Node<string>* given, string data) {
         throw std::exception("empty list!");
   
     // if the new node is the head
-
+	if (head == given) {
+		head = new Node<string>(data, head);
+		return;
+	}
 
     //standard case
     //set currentNode to head value passed in
     //loop checking currentNode next pointer 
     //  if not 'given' advance node
     Node<string>* currentNode = head;
+	while (currentNode->next != given && currentNode->next != nullptr) {
+		currentNode = currentNode->next;
+	}
 
 
     // if currentNode is nullptr -> we've reached the end, and didn't find Sam
- 
+	if (currentNode == nullptr)
+		throw std::exception("Node not found");
     //create the new node & adjust pointer
     Node<string>* newNode = new Node<string>(data, currentNode->next);
     currentNode->next = newNode;
@@ -43,7 +54,17 @@ void addBefore(Node<string>* head, Node<string>* given, string data) {
 //PRE: receives pointer to the item prior to deletion  
 //POST: deletes the next node
 void remove(Node<string>* delNext) {
+	//check for special cases
+	//if the next node is null, we can't delete it
+	if (delNext == nullptr || delNext->next == nullptr)
+		throw std::exception("Cannot delete the next node");
 
+	//set temp node to the next node
+	Node<string>* temp = delNext->next;
+	//adjust pointers
+	delNext->next = temp->next;
+	//delete the node
+	delete temp;
 
 
 }
@@ -74,6 +95,15 @@ void deleteBefore(Node<string>* head, Node<string>* given) {
     //check if the current->next->next node is the one to delete
     //if so, adjust pointers & break/return
     //otherwise advance pointer
+	while (currentNode->next->next != nullptr) {
+		if (currentNode->next->next == given) {
+			Node<string>* temp = currentNode->next;
+			currentNode->next = temp->next;
+			delete temp;
+			return;
+		}
+		currentNode = currentNode->next;
+	}
 
 
 
